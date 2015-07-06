952b7de108bfbc9f6528b9a91ad45b82c32644f5
Nicholas Merowsky <nicholas.merowsky@umassmed.edu>
2015-07-06 15:02:13 -0400

    unit testing

--- tests/test.js
+++ tests/test.js
@@ -1,3 +1,9 @@
-beforeEach(function() {
-  console.log('before every test in every file');
+var assert = require("assert")
+describe('Array', function() {
+  describe('#indexOf()', function () {
+    it('should return -1 when the value is not present', function () {
+      assert.equal(-1, [1,2,3].indexOf(5));
+      assert.equal(-1, [1,2,3].indexOf(0));
+    });
+  });
 });
